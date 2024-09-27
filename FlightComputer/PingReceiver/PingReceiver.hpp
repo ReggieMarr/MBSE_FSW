db4e2042d2160a4ea3e2c8852051fad541207974
Reg Marr <reginald.marr@reactiondynamics.space>
2024-09-27 10:31:45 -0400

    Add PingReceiver from Reference application

---
+++ FlightComputer/PingReceiver/PingReceiver.hpp
@@ -0,0 +1,17 @@
+// ======================================================================
+// PingReceiver.hpp
+// Standardization header for PingReceiver
+// ======================================================================
+
+#ifndef FlightComputer_PingReceiver_HPP
+#define FlightComputer_PingReceiver_HPP
+
+#include "FlightComputer/PingReceiver/PingReceiverComponentImpl.hpp"
+
+namespace FlightComputer {
+
+  typedef PingReceiverComponentImpl PingReceiver;
+
+}
+
+#endif
